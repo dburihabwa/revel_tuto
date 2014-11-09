6d2c1bc66492ee400fa9758ea461470b15f19d55
Thomas Durieux <durieuxthomas@hotmail.com>
2014-11-09 11:53:07 +0100

    Create the index page of the website

--- app/controllers/application.go
+++ app/controllers/application.go
@@ -2,17 +2,37 @@ package controllers
 
 import (
 	"github.com/revel/revel"
-	"going/app/routes"
+	"going/app/models"
 )
 
 type Application struct {
 	AbstractController
 }
 
+/**
+ * Render the Index page
+ */
 func (c Application) Index() revel.Result {
-	if c.connected() != nil {
-		return c.Redirect(routes.Projects.List())
+	c.connected()
+
+	// Get popular projects
+	results, err := c.Txn.Select(models.Project{},
+		`select DISTINCT p.* from Project p INNER JOIN transaction t on t.project_id=p.id WHERE p.expiration_date > NOW() GROUP BY p.id ORDER BY count(t.user_id) DESC LIMIT 3`)
+	if err != nil {
+		panic(err)
 	}
-	c.Flash.Error("Please log in first")
-	return c.Render()
+
+	var projects []*models.Project
+	for _, r := range results {
+		b := r.(*models.Project)
+		b.Pledged = 0
+		// get the pledged amount
+		results, err := c.Txn.SelectInt("select sum(amount) from transaction WHERE project_id=?", b.Id)
+		if err == nil {
+			b.Pledged = results
+		}
+		projects = append(projects, b)
+	}
+
+	return c.Render(projects)
 }
