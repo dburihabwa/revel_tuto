2a0288954d1570253f968318473a6c7cd08baa11
Thomas Durieux <durieuxthomas@hotmail.com>
2014-11-09 11:49:53 +0100

    Comments the initialisation controller

--- app/controllers/init.go
+++ app/controllers/init.go
@@ -15,19 +15,41 @@ func init() {
 	revel.InterceptMethod((*GorpController).Commit, revel.AFTER)
 	revel.InterceptMethod((*GorpController).Rollback, revel.FINALLY)
 
+	/**
+	 * Template functions
+	 */
+
+	// count the number of days between today and date
 	revel.TemplateFuncs["dayLeft"] = func(date time.Time) int {
 		today := time.Now()
 		return int(date.Sub(today).Hours() / 24)
 	}
 
+	// perform the difference between a and b
 	revel.TemplateFuncs["diff"] = func(a int64, b int64) int64 {
 		return a - b
 	}
 
-	revel.TemplateFuncs["procent"] = func(a int64, b int64) float64 {
-		return (float64(b) / float64(a)) * 100
+	// perform the percent
+	revel.TemplateFuncs["percent"] = func(total int64, current int64) int64 {
+		val := int64((float64(current) / float64(total)) * 100)
+		if val < 0 {
+			return 0
+		}
+		return val
 	}
-
+	// perform the percent with and limit the percent to a maximum value
+	revel.TemplateFuncs["percentMax"] = func(total int64, current int64, max int64) int64 {
+		val := int64((float64(current) / float64(total)) * 100)
+		if val < 0 {
+			return 0
+		}
+		if val > max {
+			return max
+		}
+		return val
+	}
+	// convert markdown to HTML
 	revel.TemplateFuncs["md"] = func(markdown string) T.HTML {
 		unsafe := blackfriday.MarkdownCommon([]byte(markdown))
 		html := bluemonday.UGCPolicy().SanitizeBytes(unsafe)
