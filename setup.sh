6e4969afdf290966400dbb5b9ad68796992fb4a0
val-old <val-old@ubuntu.(none)>
2014-11-10 00:04:49 +0100

    update setup

--- setup.sh
+++ setup.sh
@@ -1,26 +1,21 @@
 #! /bin/bash
-
 ROOT=~/
 # ROOT=/gfs/$(whoami)/
-
 echo "mkdir ~/gocode"
-mkdir $(ROOT)/gocode
+mkdir $ROOT/gocode
 touch ~/.gorc
-GOPATH=$(ROOT)/gocode
+GOPATH=$ROOT/gocode
 export GOPATH=$GOPATH
 echo "export GOPATH=$GOPATH" > ~/.gorc
-
 echo "go get github.com/revel/revel"
 go get github.com/revel/revel
 echo "go get github.com/revel/cmd/revel"
 go get github.com/revel/cmd/revel
-
 export PATH=$PATH:$GOPATH/bin
 echo "export PATH=$PATH" >> ~/.gorc
 echo "" >> ~/.bashrc
 echo "# GO CODE" >> ~/.bashrc
 echo "source ~/.gorc" >> ~/.bashrc
-
 # Download code
 cd $GOPATH/src
 git clone https://github.com/dburihabwa/revel_tuto going
